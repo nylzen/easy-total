cb9836b70aa6c21c294df536a284b2722e4977cf
Nelson <tugoresn@gmail.com>
2024-06-08 02:11:03 -0300

    add: LS products & copy list to clipboard

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,11 +4,18 @@ import { useEffect, useRef, useState } from "react";
 import { Total } from "../Total/Total";
 import { ProductForm } from "../ProductForm/ProductForm";
 import { ProductList } from "../ProductList/ProductList";
+import { Toaster, toast } from "sonner";
 
 export const Form = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(
+    JSON.parse(localStorage.getItem("products")) || [],
+  );
   const [total, setTotal] = useState(0);
   const nameInputRef = useRef(null);
+
+  const saveToLocalStorage = (products) => {
+    localStorage.setItem("products", JSON.stringify(products));
+  };
 
   useEffect(() => {
     const calculateTotal = () => {
@@ -23,10 +30,12 @@ export const Form = () => {
     };
 
     calculateTotal();
+    saveToLocalStorage(products);
   }, [products]);
 
   const addProduct = (product) => {
     setProducts((prevProducts) => [...product, ...prevProducts]);
+    saveToLocalStorage([...product, ...products]);
   };
 
   const updateProductQuantity = (index, quantity) => {
@@ -39,6 +48,30 @@ export const Form = () => {
     setProducts(newProducts);
   };
 
+  const generateMessage = () => {
+    const date = new Date().toLocaleDateString("es-AR");
+    let message = `Lista de productos EasyTotal - ${date}\n\n`;
+    products.forEach((product) => {
+      message += `${product.name} x ${product.quantity} - $${product.price}\n`;
+    });
+    message += `\nTotal: $${total.toLocaleString("es-AR")}`;
+    console.log(message);
+    return message;
+  };
+
+  const handleCopyToClipboard = () => {
+    const message = generateMessage();
+    navigator.clipboard.writeText(message).then(
+      () => {
+        toast.success("Lista copiada al portapapeles.");
+      },
+      (err) => {
+        toast("Error al copiar el mensaje.", { type: "error" });
+        console.error("Error al copiar el mensaje: ", err);
+      },
+    );
+  };
+
   return (
     <section className=" pb-20 flex flex-col h-screen bg-gray-950 text-gray-50">
       <div className="md:max-w-[800px] container mx-auto px-4 py-8 flex-1 overflow-auto">
@@ -48,7 +81,16 @@ export const Form = () => {
           products={products}
           updateProductQuantity={updateProductQuantity}
         />
-        <Total total={total} />
+        <div className="bg-gray-800 py-6 px-6 flex items-center justify-center absolute w-full bottom-0 left-0">
+          <Total total={total} />
+          <Toaster position="bottom-center" richColors closeButton/>
+          <button
+            onClick={handleCopyToClipboard}
+            className="bg-green-500 text-white px-4 py-2 rounded-md"
+          >
+            Copiar
+          </button>
+        </div>
       </div>
     </section>
   );
--- src/components/Total/Total.jsx
+++ src/components/Total/Total.jsx
@@ -1,10 +1,10 @@
 export const Total = ({ total }) => {
   return (
-    <div className="bg-gray-800 py-6 px-6 flex items-center justify-between absolute w-full bottom-0 left-0">
-      <div className="max-w-[800px] mx-auto flex items-center justify-between w-full">
-        <h2 className="font-meidum">Total</h2>
-        <p className="font-bold">${total.toLocaleString('es-AR')}</p>
-      </div>
+    <div className="max-w-[800px] mx-auto flex items-center justify-between w-full">
+      <h2 className="font-medium flex items-center gap-x-3 ">
+        Total: <span className="font-bold text-xl"> ${total.toLocaleString("es-AR")}</span>
+      </h2>
+      {/* <p className="font-bold">${total.toLocaleString("es-AR")}</p> */}
     </div>
   );
 };
